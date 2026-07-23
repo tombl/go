1869d795d99a72f4801ad22cb160f8755b4bd41c
Thomas Stokes <git@tombl.dev>
2026-07-23 09:06:22 +0800

    wasm: add linux target scaffolding

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -1806,6 +1806,7 @@ var cgoEnabled = map[string]bool{
 	"linux/riscv64":   true,
 	"linux/s390x":     true,
 	"linux/sparc64":   true,
+	"linux/wasm":      false,
 	"android/386":     true,
 	"android/amd64":   true,
 	"android/arm":     true,
